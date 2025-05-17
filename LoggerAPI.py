8faddd82b0caf3429b7a662ba92a189caed75cb6
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-17 09:17:09 +0200

    Commit created table in DynamoDb and working on API calls

--- LoggerAPI.py
+++ LoggerAPI.py
@@ -1,3 +1,33 @@
-import requests
+from boto3 import resource
+from datetime import datetime
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Logs')
+
+def send_log(level, message):
+    item = {
+        'log_id': str(uuid.uuid4()),
+        'timestamp': datetime.timezone.now().isoformat(),
+        'level': level,
+        'message': message
+    }
+    table.put_item(Item=item)
+
+def get_log(log_id):
+    try:
+        response = table.get_Item(
+            Key = {
+                'log_id': log_id
+            }
+        )
+        return response.get('Item')
+    except Exception as e:
+        print(f"Error fetching log: {e}")
+        return None
+
+    
+
 
 #Send logging data to database
+#Function to get logs
