18f4d5d00813014c7f925ec8f07cdb866ec4c16e
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-19 08:37:57 +0200

    Commit will implement fixed code tommorow

---
+++ ConfigCognito.py
@@ -0,0 +1,4 @@
+REGION = "eu-north-1"
+CLIENT_ID = "59dqniop3rslpov6lm5hti8tl"
+USER_POOL_ID = "eu-north-1_cjgmxdbME"
+JWK_URL = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json"
