f0058a63cc3454163e7841a25cea76b87ce0f02b
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-28 06:33:31 +0200

    Commit added ApiTable file

--- app/frontend/src/pages/Dashboard.jsx
+++ app/frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from "react-oidc-context";
 import { useNavigate } from "react-router-dom";
+
 
 function Dashboard() {
   const auth = useAuth();
   const navigate = useNavigate();
   
+  /*Sign out function*/
   const signOut = () => {
     auth.removeUser();
     navigate('/');
@@ -17,9 +20,15 @@ function Dashboard() {
       <h2>Välkommen, {auth.user?.profile.email}!</h2>
       <p>Du är nu inloggad och kan se din dashboard.</p>
   
+    
     <div className="api-section">
       <h3>API Data</h3>
-      <p>Här kommer data från ditt backend att visas...</p>
+
+      <div>
+        {logs.map((logs) => (
+          <img key={logs.id} src={logs.url} alt={logs.title} />
+      ))}
+      </div>
     </div>
   
       <button className="btn-logout" onClick={signOut}>Logga ut</button>
