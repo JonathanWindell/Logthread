f137a9efd271e9931bdaf8d599c5cca6d02612f3
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-28 07:32:46 +0200

    API calls are working in the frontend! Will begin designing the dashboard

--- app/backend/main.py
+++ app/backend/main.py
@@ -2,11 +2,20 @@ from app.backend.logging.Logger import Logger
 from app.backend.logging.LoggerEnum import LoggerEnum
 from app.backend.service.SnsHandler import send_critical_notification
 from app.backend.logging.Routers import router as logs_router
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(logs_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 @app.get("/")
 def read_root():
