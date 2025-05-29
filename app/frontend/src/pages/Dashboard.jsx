ec4398972a401996294abe5cef164e0008633efa
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-29 06:29:45 +0200

    Commit creted JSX file and structure in frontend for log distribution

--- app/frontend/src/pages/Dashboard.jsx
+++ app/frontend/src/pages/Dashboard.jsx
@@ -20,22 +20,32 @@ function Dashboard() {
     <div className="dashboard-container">
       <h1>Dashboard</h1>
       <h2>Välkommen, {auth.user?.profile.email}!</h2>
-      <p>Du är nu inloggad och kan se din dashboard.</p>
+      <p>Log Dashboard</p>
   
-    
-    <div className="api-section">
-      <h3>Log Feed</h3>
-      <div>
-        <ApiTable />
+      <div className="api-section">
+        <h3>Log Feed</h3>
+          <div>
+            <ApiTable />
+          </div>
       </div>
-    </div>
-    <div className="critical-logs">
-      <h3>Critical Logs</h3>
-      <div>
-        <CriticalApiTable />
+      <div className="critical-logs">
+        <h3>Critical Logs</h3>
+          <div>
+            <CriticalApiTable />
+          </div>
+      </div> 
+            
+      <div className="log-distribution">
+        <h3>Log Distribution</h3>
+          <div className="distribution-container">
+            <p>Debug</p>
+            <p>Info</p>
+            <p>Warning</p>
+            <p>Error</p>
+            <p>Critical</p>
+          </div>
       </div>
-      
-    </div>
+
       <button className="btn-logout" onClick={signOut}>Logga ut</button>
     </div>
   );
