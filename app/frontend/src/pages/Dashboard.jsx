b504a2d22b4d84010942adc77a8ea9006912fedd
Jonathan Windell <windelljonathan04@gmail.com>
2025-05-29 05:59:03 +0200

    Commit CRIITICAL endpoint is working

--- app/frontend/src/pages/Dashboard.jsx
+++ app/frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useAuth } from "react-oidc-context";
 import { useNavigate } from "react-router-dom";
 import ApiTable from './ApiTable'
+import CriticalApiTable from './CriticalApiTable'
 
 
 function Dashboard() {
@@ -30,6 +31,9 @@ function Dashboard() {
     </div>
     <div className="critical-logs">
       <h3>Critical Logs</h3>
+      <div>
+        <CriticalApiTable />
+      </div>
       
     </div>
       <button className="btn-logout" onClick={signOut}>Logga ut</button>
