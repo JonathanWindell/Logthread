99455bde2506196c1017909fe74b8d9d2121fca8
Jonathan Windell <windelljonathan04@gmail.com>
2025-06-01 13:35:26 +0200

    Commit structure in dashboard soon finished. Added export button aswell but will have to fix some small changes

--- app/frontend/src/pages/Dashboard.jsx
+++ app/frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import ApiTable from './ApiTable'
 import CriticalApiTable from './CriticalApiTable'
 import DistributionApiTable from './DistributionApiTable';
+import Exporting from './Exporting';
 
 
 function Dashboard() {
@@ -19,32 +20,38 @@ function Dashboard() {
 
   return (
     <div className="dashboard-container">
+    <div className="dashboard-header">
       <h1>Dashboard</h1>
+    </div>
+    
+    <div className="welcome-message">
       <h2>Welcome, {auth.user?.profile.email}!</h2>
       <p>Log Dashboard</p>
-  
-      <div className="api-section">
-        <h3>Log Feed</h3>
-          <div>
-            <ApiTable />
-          </div>
-      </div>
-      <div className="critical-logs">
-        <h3>Critical Logs</h3>
-          <div>
-            <CriticalApiTable />
-          </div>
-      </div> 
-            
-      <div className="log-distribution">
-        <h3>Log Distribution</h3>
-          <div className="distribution-container">
-            <DistributionApiTable />
-          </div>
-      </div>
+    </div>
 
+    <div className="Exporting">
+      <Exporting />
+    </div>
+
+    <div className="critical-logs critical-table">
+      <h3>Critical Logs</h3>
+      <CriticalApiTable />
+    </div>
+    
+    <div className="log-distribution stats-table">
+      <h3>Log Distribution</h3>
+      <DistributionApiTable />
+    </div>
+
+    <div className="api-section log-table">
+      <h3>Log Feed</h3>
+      <ApiTable />
+    </div>
+
+    <div className="logout-section">
       <button className="btn-logout" onClick={signOut}>Logga ut</button>
     </div>
+  </div>
   );
 }
 
