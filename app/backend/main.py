bace336a938465286e42aa98a3bad654f5fd4965
Jonathan Windell <windelljonathan04@gmail.com>
2025-07-14 08:46:27 +0200

    Commit

--- app/backend/main.py
+++ app/backend/main.py
@@ -4,6 +4,7 @@ from app.backend.service.SnsHandler import send_critical_notification
 from app.backend.apifiles.Routers import router as logs_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from mangum import Mangum  
 
 app = FastAPI()
 
@@ -13,27 +14,16 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
 def read_root():
     return {"message": "Loggify API is running!"}
 
-def test_logging():
-    # Create logger instance
-    logger = Logger(level=LoggerEnum.DEBUG)
-    
-    #Log to dynamoDB
-    #logger.log("Testmeddelande: DEBUG-nivå", LoggerEnum.DEBUG)
-    
-    #Log to dynamoDB and send mail
-    #logger.log("KRITISKT FEL: Systemkrasch", LoggerEnum.CRITICAL)
-
-    #send_critical_notification("Manuellt test av SNS")
-
 if __name__ == "__main__":
     import uvicorn
-    test_logging()
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+handler = Mangum(app)
